96cf742c3cf866007af4b8e367d0154a5b10524d
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-04-04 19:19:17 +0200

    cmake: use conan cmake_find_package generator

--- cmake/Conan.cmake
+++ cmake/Conan.cmake
@@ -1,4 +1,5 @@
-macro(run_conan)
+function(run_conan)
+cmake_parse_arguments(RC "" "ALL" "" ${ARGN})
 # Download automatically, you can also just copy the conan.cmake file
 if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
   message(
@@ -13,17 +14,33 @@ include(${CMAKE_BINARY_DIR}/conan.cmake)
 conan_add_remote(NAME bincrafters URL
                  https://api.bintray.com/conan/bincrafters/public-conan)
 
+set(REQUIREMENTS
+  docopt.cpp/0.6.2
+  imgui-sfml/2.1@bincrafters/stable
+)
+
+if(RC_ALL)
+  list(APPEND REQUIREMENTS
+    catch2/2.11.0
+    fmt/6.1.2
+    spdlog/1.5.0
+  )
+endif()
+
+
 conan_cmake_run(
   REQUIRES
-  ${CONAN_EXTRA_REQUIRES}
-  catch2/2.11.0
-  docopt.cpp/0.6.2
-  fmt/6.0.0
-  spdlog/1.5.0
+    ${CONAN_EXTRA_REQUIRES}
+    ${REQUIREMENTS}
   OPTIONS
-  ${CONAN_EXTRA_OPTIONS}
+    ${CONAN_EXTRA_OPTIONS}
   BASIC_SETUP
-  CMAKE_TARGETS # individual targets to link to
   BUILD
-  missing)
-endmacro()
+    missing
+  GENERATORS
+    cmake_find_package  # Create FindXXX.cmake cmake modules
+)
+
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_BINARY_DIR}")
+set(CMAKE_MODULE_PATH "${CMAKE_MODULE_PATH}" PARENT_SCOPE)
+endfunction()
--- cmake/StandardProjectSettings.cmake
+++ cmake/StandardProjectSettings.cmake
@@ -15,7 +15,7 @@ if(CCACHE)
   message("using ccache")
   set(CMAKE_CXX_COMPILER_LAUNCHER ${CCACHE})
 else()
-  message("ccache not found cannot use")
+  message(STATUS "ccache not found cannot use")
 endif()
 
 # Generate compile_commands.json to make it easier to work with clang based
